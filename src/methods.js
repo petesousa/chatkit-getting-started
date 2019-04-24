c91e29852638f9b22dedf30b72a4a42afcd5fd7d
Pete Sousa <im.pete.sousa@gmail.com>
2019-04-23 23:52:06 -0300

    chatkit getting starter first session

---
+++ src/methods.js
@@ -0,0 +1,124 @@
+import Chatkit from '@pusher/chatkit-client';
+import axios from 'axios';
+
+function handleInput(event) {
+  const { value, name } = event.target;
+
+  this.setState({
+    [name]: value,
+  });
+}
+
+function connectToRoom(id =20806224) {
+  const { currentUser } = this.state;
+
+  this.setState({
+    messages: [],
+  });
+
+  return currentUser
+    .subscribeToRoom({
+      roomId: `${id}`,
+      messageLimit: 100,
+      hooks: {
+        onMessage: message => {
+          this.setState({
+            messages: [...this.state.messages, message],
+          });
+        },
+        onPresenceChanged: () => {
+          const { currentRoom } = this.state;
+          this.setState({
+            roomUsers: currentRoom.users.sort(a => {
+              if (a.presence.state === 'online') return -1;
+
+              return 1;
+            }),
+          });
+        },
+      },
+    })
+    .then(currentRoom => {
+      const roomName =
+        currentRoom.customData && currentRoom.customData.isDirectMessage
+          ? currentRoom.customData.userIds.filter(
+              id => id !== currentUser.id
+            )[0]
+          : currentRoom.name;
+
+      this.setState({
+        currentRoom,
+        roomUsers: currentRoom.users,
+        rooms: currentUser.rooms,
+        roomName,
+      });
+    })
+    .catch(console.error);
+}
+
+
+
+
+function connectToChatkit(event) {
+  event.preventDefault();
+
+  const { userId } = this.state;
+
+  if (userId === null || userId.trim() === '') {
+    alert('Invalid userId');
+    return;
+  }
+
+  axios
+    .post('http://localhost:5200/users', { userId })
+    .then(() => {
+      const tokenProvider = new Chatkit.TokenProvider({
+        url: 'http://localhost:5200/authenticate',
+      });
+
+      const chatManager = new Chatkit.ChatManager({
+        instanceLocator: 'v1:us1:8fc1a71c-9b82-435b-b481-3dfce2831cd9',
+        userId,
+        tokenProvider,
+      });
+
+      return chatManager
+        .connect({
+          onAddedToRoom: room => {
+            const { rooms } = this.state;
+            this.setState({
+              rooms: [...rooms, room],
+            });
+          },
+        })
+        .then(currentUser => {
+          this.setState(
+            {
+              currentUser,
+              showLogin: false,
+              rooms: currentUser.rooms,
+            },
+            () => connectToRoom.call(this)
+          );
+        });
+    })
+    .catch(console.error);
+}
+
+function sendMessage(event) {
+  event.preventDefault();
+  const { newMessage, currentUser, currentRoom } = this.state;
+
+  if (newMessage.trim() === '') return;
+
+  currentUser.sendMessage({
+    text: newMessage,
+    roomId: `${currentRoom.id}`,
+  });
+
+  this.setState({
+    newMessage: '',
+  });
+}
+
+export { handleInput, connectToRoom, connectToChatkit, sendMessage }
